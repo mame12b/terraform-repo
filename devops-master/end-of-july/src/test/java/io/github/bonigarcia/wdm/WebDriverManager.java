fbaa31e980fe9b461735b12815467fc8c6fca33b
mame12b <mame4005@gmail.com>
2025-08-02 17:16:26 +0400

    adding end of july file

---
+++ devops-master/end-of-july/src/test/java/io/github/bonigarcia/wdm/WebDriverManager.java
@@ -0,0 +1,12 @@
+package io.github.bonigarcia.wdm;
+
+import test.BasicValidation1;
+
+public class WebDriverManager {
+
+	public static BasicValidation1 chromedriver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
