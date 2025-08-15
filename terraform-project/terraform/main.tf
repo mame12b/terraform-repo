58873d332cdb6f54904e2295c3ca2deb795bc388
mame12b <mame4005@gmail.com>
2025-08-15 20:39:34 +0400

    terraform data added

--- terraform-project/terraform/main.tf
+++ terraform-project/terraform/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 }
